8c36516c2dfeaac1118c2d89c051fa7255f2f71b
maimoh <maimoh85349@gmail.com>
2025-04-13 22:54:45 +0200

    Update main.m

--- main.m
+++ main.m
@@ -9,10 +9,15 @@ t_final = 24;
 f = @(t, C) -k * C;   
 
 
-[t1, y1] = Euler(f, 0, t_final, C0, h);
+[t1, y1] = EulerForward(f, 0, t_final, C0, h);
 [t2, y2] = Midpoint(f, 0, t_final, C0, h);
 [t3, y3] = rung_cutto(f, 0, t_final, C0, h);
+[t4, y4] = HeunsMethod(f, 0, t_final, C0, h);
+[t5, y5] = adams_bashforth(f, 0, t_final, C0, h);
+[t6, y6] = lmm_adams_moulton(f, 0, t_final, C0, h);
+[t7, y7] = EulerBackward(f, 0, t_final, C0, h);
 
 
-plot-connection(t1, y1, y2, y3);   
+plot_connection(t1,y1,y2,y3,y4,y5,y6,y7);
+   drawnow;  
 
